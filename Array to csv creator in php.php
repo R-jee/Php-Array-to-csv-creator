2f48676141c2b61ff5800c7168eace4a5519ff08
Rana Umair <rjee4631@gmail.com>
2022-07-20 11:03:32 +0500

    Create Array to csv creator in php.php

---
+++ Array to csv creator in php.php
@@ -0,0 +1,33 @@
+
+function convert_multiline_toSingleLine_text($s){
+    return str_replace(array("\r","\n"),"", trim($s) );
+}
+function CREATE_CSV_FEEDS___Generic_function( $array , $separated_By){
+    $csv = '';
+    $separated_By_char = "";
+    
+    if($separated_By == "comma" ){
+        $separated_By_char = " , ";
+    }if($separated_By == "tab" ){
+        $separated_By_char = "   ";
+    }if($separated_By == "semi_colon" ){
+        $separated_By_char = " ; ";
+    }if($separated_By == "pipe" ){
+        $separated_By_char = " | ";
+    }
+
+    $header = false;
+    foreach ($array as $line) {	
+        if (empty($header)) {
+            $header = array_keys($line);
+            $csv .= implode( $separated_By_char , $header);
+            $header = array_flip($header);		
+        }
+        $line_array = array();
+        foreach($line as $value) {
+            array_push($line_array,  convert_multiline_toSingleLine_text($value) );
+        }
+        $csv .= "\n" .  implode( $separated_By_char, $line_array) ;
+    }
+    return $csv ;
+}
